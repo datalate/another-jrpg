537e1c766417714ea4a1650ff9399219d4c18f28
Lauri Haavisto <lauri.haavisto@miemala.com>
2017-07-20 21:16:21 +0300

    Add SDL2 and project files

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 
-int main() {
+#include <SDL.h>
+
+int main(int argc, char *argv[]) {
 	std::cout << "Hello, world!" << std::endl;
+
+	if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
+		std::cout << "SDL_Init() failed: " << SDL_GetError() << std::endl;
+
+		return 1;
+	}
 
 	return 0;
 }
