6863eb7af7ff3687ec4dfab56977e854fc7d73cc
Lauri Haavisto <lauri.haavisto@miemala.com>
2018-08-09 02:53:32 +0300

    Create file open/parse logic in Level editor

--- level-editor/mainwindow.cc
+++ level-editor/mainwindow.cc
@@ -3,13 +3,16 @@
 #include "editorscene.hh"
 #include <QDebug>
 #include <QAction>
+#include <QFileDialog>
+#include <QString>
+#include "yaml-cpp/yaml.h"
 
-const unsigned int EDITOR_WIDTH{25};
-const unsigned int EDITOR_HEIGHT{20};
+const unsigned int DEFAULT_EDITOR_WIDTH{25};
+const unsigned int DEFAULT_EDITOR_HEIGHT{20};
 
 MainWindow::MainWindow(QWidget* parent) :
     QMainWindow(parent),
-    ui(new Ui::MainWindow), editor_{EDITOR_WIDTH, EDITOR_HEIGHT}, activeTile_{nullptr} {
+    ui(new Ui::MainWindow), editor_{DEFAULT_EDITOR_WIDTH, DEFAULT_EDITOR_HEIGHT}, activeTile_{nullptr} {
 
     ui->setupUi(this);
 
@@ -34,6 +37,8 @@ MainWindow::MainWindow(QWidget* parent) :
 
     success = connect(ui->actionNew, &QAction::triggered, this, &MainWindow::fileNew);
     Q_ASSERT(success);
+    success = connect(ui->actionOpen, &QAction::triggered, this, &MainWindow::open);
+    Q_ASSERT(success);
     success = connect(ui->actionExit, &QAction::triggered, this, &MainWindow::close);
     Q_ASSERT(success);
 }
@@ -43,7 +48,60 @@ MainWindow::~MainWindow() {
 }
 
 void MainWindow::fileNew() {
-    editor_.createNew(EDITOR_WIDTH, EDITOR_HEIGHT);
+    editor_.createNew(DEFAULT_EDITOR_WIDTH, DEFAULT_EDITOR_HEIGHT);
+}
+
+void MainWindow::open() {
+    const QString filePath{QFileDialog::getOpenFileName(this, "Open", QString(), "*.yml")};
+    QString errorMsg;
+    bool ok{true};
+    YAML::Node node;
+
+    if (!YamlParser::loadFile(filePath.toStdString(), node)) {
+        errorMsg = "Failed to open/parse file";
+        ok = false;
+    }
+    else {
+        std::string name;
+        unsigned int width{0};
+        unsigned int height{0};
+        std::vector<unsigned int> tileData;
+
+        YamlParser::parse<std::string>(node["name"], name);
+        YamlParser::parse<unsigned int>(node["width"], width);
+        YamlParser::parse<unsigned int>(node["height"], height);
+        YamlParser::parse<std::vector<unsigned int>>(node["tiles"], tileData);
+
+        if (name.empty() || width == 0 || height == 0 || tileData.empty()) {
+            errorMsg = "Key attributes missing";
+            ok = false;
+        }
+        else if (tileData.size() != width * height) {
+            errorMsg = "Incorrect number of tiles";
+            ok = false;
+        }
+        else {
+            std::vector<TileItem*> tiles;
+            unsigned int x{0};
+            unsigned int y{0};
+            for (const auto& tile : tileData) {
+                tiles.push_back(new TileItem(x, y, tile, this));
+                ++x;
+
+                if (x == width) {
+                    x -= width;
+                    ++y;
+                }
+            }
+        }
+
+        if (!ok) {
+            ui->statusBar->showMessage(tr("Failed to open level '%1': %2").arg(filePath).arg(errorMsg));
+        }
+        else {
+            ui->statusBar->showMessage(tr("Opened file '%1'").arg(filePath));
+        }
+    }
 }
 
 void MainWindow::updateTile(TileItem *tile) {
--- level-editor/mainwindow.hh
+++ level-editor/mainwindow.hh
@@ -18,11 +18,12 @@ class MainWindow : public QMainWindow {
     Q_OBJECT
 
 public:
-    explicit MainWindow(QWidget* parent = 0);
+    explicit MainWindow(QWidget* parent = nullptr);
     ~MainWindow();
 
 private slots:
     void fileNew();
+    void open();
     void updateTile(TileItem* tile);
     void setActiveTile(TileItem* tile);
 
